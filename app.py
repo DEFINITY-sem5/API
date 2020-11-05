6d183cf67a87cf83d71d953eaf2c7b386623d264
ashok-mbp16 <ashok_a380@icloud.com>
2020-11-05 11:11:49 +0530

    warnings

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import Flask
 from newspaper import Article
 import numpy
 from threading import Thread
+import urllib3
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return("Build Commencing")
+model_cb = None
+model_fn = None
+
+urllib3.disable_warnings()
+np.warnings.filterwarnings('ignore')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
