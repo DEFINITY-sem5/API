9341cbbd2a800db90a92d412aab0205ab46bdb68
ashok-mbp16 <ashok_a380@icloud.com>
2020-11-05 09:47:27 +0530

    minor changes to check functionality

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return("Build Commencing")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
