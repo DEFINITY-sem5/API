c5cbe6a0ce8e5f6704b3c834bb807207d80760f2
ashok-mbp16 <ashok_a380@icloud.com>
2020-11-06 12:40:49 +0530

    globalising var

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from newspaper import Article
 import re
+import functools
 import flask
 import urllib3
 from threading import Thread
@@ -22,7 +23,7 @@ Load_ML()
 urllib3.disable_warnings()
 np.warnings.filterwarnings('ignore')
 
-
+data = {"success": False}
 #to look into URL's
 def read(url):
     article = Article(url, fetch_images = False)
@@ -32,13 +33,22 @@ def read(url):
     article_text = article.text
     return(article_title, article_text)
 
+
+@functools.lru_cache(maxsize=512, typed=False)
 @app.route("/predict", methods=["POST"])
 
 def predict():
+
+    global model_cb, model_fn
+    global data
+
+    data = {"success" : False}
+
     if flask.request.method == "POST":
         url = flask.request.args.get("url")
         article_title, article_text = read(url)
 
+        threads=[]
         
         
 if __name__ == "__main__":
