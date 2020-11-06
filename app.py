de6d3d67566c550740057c128f46dbb93a5fff57
ashok-mbp16 <ashok_a380@icloud.com>
2020-11-06 15:26:40 +0530

    test

--- app.py
+++ app.py
@@ -20,8 +20,17 @@ def Load_ML():
 
 Load_ML()
 
+def pred_clickbait(input_):
+    global model_clickbait, data
+    data["clickbait"] = model_cb.predict(input_)
+
+def pred_fakenews(input_):
+    global model_profile, data
+    data["article_profile"] = model_fn.predict(input_) 
+
 urllib3.disable_warnings()
 np.warnings.filterwarnings('ignore')
+
 
 data = {"success": False}
 #to look into URL's
@@ -45,12 +54,31 @@ def predict():
     data = {"success" : False}
 
     if flask.request.method == "POST":
-        url = flask.request.args.get("url")
+        #url = flask.request.args.get("url")
+        url = "https://timesofindia.indiatimes.com/city/mumbai/no-evidence-to-show-juhu-cops-intended-to-cause-death-court/articleshow/79074605.cms"
         article_title, article_text = read(url)
 
         threads=[]
+
+        if article_title is not None:
+            t = Thread(target=pred_fakenews, args=([article_text]))
+            threads.append(t)
+            t.start
         
-        
+        if article_text is not None:
+            article_title = article_title.replace("%20", " ")
+            data["article_text"] = article_title
+
+            t = Thread(target=pred_clickbait, args=([article_title]))
+            threads.append(t)
+            t.start
+
+
+        [t.join() for t in threads]
+        data["success"] = True
+
+    return flask.jsonify(data)
+
 if __name__ == "__main__":
     print("Starting Flask server")
     app.run(host='0.0.0.0', port=5000)
--- classifier.py
+++ classifier.py
@@ -7,6 +7,7 @@ import pickle
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from base import *
+import functools
 
 class Fakenews_classifier(api_model):
     def __init__(self, debug=True):
@@ -26,3 +27,36 @@ class Clickbait_classifier():
         self.classes = ['clickbait', 'not-clickbait']
         with open("saved_models/Clickbait_savedmodel/clickbait_tokenizer.pickle",'rb') as cb_handle:
             self.tkn = pickle.load(cb_handle)
+        if self.debug:
+            if self.run_self_test():
+                print(" * [i] Model: " + self.name +
+                      " has loaded successfully")
+            else:
+                print(" * [!] An error has occured in self test!!")
+
+    @functools.lru_cache(maxsize=512, typed=False)
+    def predict(self, input_data):
+        processed_input = self.preprocess(input_data)
+        preds = self.model.predict(processed_input)
+        pred = preds.argmax(axis=-1)
+
+        output = self.classes[pred[0]]
+
+        if self.debug:
+            print(output)
+
+        return output
+
+    def preprocess(self, input_data):
+        input_string = str(input_data).lower()
+        input_string = re.sub(r'[^\w\s]', '', input_string)
+
+        input_token = self.tokenizer.texts_to_sequences([input_string])
+        output_t = pad_sequences(input_token, padding='pre', maxlen=(15))
+        processed_input = pad_sequences(output_t, padding='post', maxlen=(20))
+
+        if self.debug:
+            print(" * [d] Cleaned string", input_string)
+            print(" * [d] Test sequence", processed_input)
+
+        return processed_input    
